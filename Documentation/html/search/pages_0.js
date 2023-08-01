4933cb34846fa404482e0c4a64c17869f2c056ac
Serhii Nesteruk <yarench228@gmail.com>
2023-08-01 18:54:39 +0200

    add documentation

--- Documentation/html/search/pages_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['console_2dgame_2dbasis_0',['Console-Game-Basis',['../md__r_e_a_d_m_e.html',1,'']]],
-  ['contributor_20covenant_20code_20of_20conduct_1',['Contributor Covenant Code of Conduct',['../md__c_o_d_e___o_f___c_o_n_d_u_c_t.html',1,'']]]
-];
